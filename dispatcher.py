2b8e58f97e9fd1395889b1b46bc5c1274208c73c
Alexey Vymyatnin <vymyatnin_av@tomsk.lemz.ru>
2018-12-03 10:51:51 +0700

    Dispatcher refactoring

--- dispatcher.py
+++ dispatcher.py
@@ -28,45 +28,31 @@ class Dispatcher:
     def addModule(self, name):
         self.modArray.append(name)
 
+    def exec(self, mod, cmd):
+        className = eval(mod[0:].capitalize())
+        try:
+            if cmd == 'init':
+                getattr(className, cmd)(className, mod, self.config[mod])
+            else:
+                getattr(className, cmd)(className)
+        except AttributeError:
+            raise NotImplementedError("Class `{}` does not implement `{}`".format(
+                'className'.__class__.__name__, 'init'))
+
     def initModules(self):
         for mod in self.modArray:
-            className = eval(mod[0:].capitalize())
-
-            try:
-                getattr(className, 'init')(className, mod, self.config[mod])
-            except AttributeError:
-                raise NotImplementedError("Class `{}` does not implement `{}`".format(
-                    'className'.__class__.__name__, 'init'))
+            self.exec(mod, 'init')
 
     def startModules(self):
         for mod in self.modArray:
-            className = eval(mod[0:].capitalize())
-
-            try:
-                getattr(className, 'start')(className)
-            except AttributeError:
-                raise NotImplementedError("Class `{}` does not implement `{}`".format(
-                    'className'.__class__.__name__, 'start'))
+            self.exec(mod, 'start')
 
     def loopModules(self):
         for mod in self.modArray:
-            className = eval(mod[0:].capitalize())
-
-            try:
-                getattr(className, 'loop')(className)
-            except AttributeError:
-                raise NotImplementedError("Class `{}` does not implement `{}`".format(
-                    'className'.__class__.__name__, 'loop'))
+            self.exec(mod, 'loop')
 
     def stopModules(self):
-        for mod in self.modArray:
-            className = eval(mod[0:].capitalize())
-
-            try:
-                getattr(className, 'stop')(className)
-            except AttributeError:
-                raise NotImplementedError("Class `{}` does not implement `{}`".format(
-                    'className'.__class__.__name__, 'stop'))
+        self.exec(mod, 'stop')
 
     def printModules(self):
         for mod in self.modArray:
